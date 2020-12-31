7dd1a0c6c3e4995ff1a8019ba5f088c4744f9312
Valeria Avila <valeria.avila17@gmail.com>
2020-12-31 06:52:43 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MillionMealsGoldLeaf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

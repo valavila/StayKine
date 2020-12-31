2b0cbce18af9d1da5e9e3f15c69254b97da2f172
Valeria Avila <valeria.avila17@gmail.com>
2020-12-31 09:49:08 -0800

    Added DB comtext to project & startup

---
+++ Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MillionMealsGoldLeaf.Models
+{
+    public class Email
+    {
+        [Key]
+        public int id { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        public bool isSubscribed { get; set; }
+    }
+}
